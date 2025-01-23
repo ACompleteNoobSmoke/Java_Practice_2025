144ad681dae2c14aef5127d593e8d9aed1a56b32
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2025-01-23 04:15:00 -0600

    Generic Method

--- src/main/java/org/generic/GenericClass.java
+++ src/main/java/org/generic/GenericClass.java
@@ -17,11 +17,22 @@ public class GenericClass<T> {
     }
 
     public static void main(String[] args) {
-        Customer newCustomer =
-                new Customer("Osaretin", "oo@gmail", "8989", LocalDate.of(1995, 3,19));
-        GenericClass<Customer> customerGenericClass = new GenericClass<>(newCustomer);
-        System.out.println(customerGenericClass);
-        GenericClass<Integer> integerGenericClass = new GenericClass<>(20);
-        System.out.println(integerGenericClass);
+//        Customer newCustomer =
+//                new Customer("Osaretin", "oo@gmail", "8989", LocalDate.of(1995, 3,19));
+//        GenericClass<Customer> customerGenericClass = new GenericClass<>(newCustomer);
+//        System.out.println(customerGenericClass);
+//        GenericClass<Integer> integerGenericClass = new GenericClass<>(20);
+//        System.out.println(integerGenericClass);
+
+        String[] games = {"SSX 3", "Burnout 3", "UFC Undisputed 3"};
+        Character[] letters = {'A', 'B', 'C'};
+        print(games);
+        print(letters);
+    }
+
+    static <T> void print (T[] array) {
+        for (T type: array) {
+            System.out.println(type.getClass().getName() + " - " + type);
+        }
     }
 }

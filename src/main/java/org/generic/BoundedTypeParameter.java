1327103841cef64348b79227c890b0efe71aef01
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2025-01-23 04:27:28 -0600

    BoundedType test

--- src/main/java/org/generic/BoundedTypeParameter.java
+++ src/main/java/org/generic/BoundedTypeParameter.java
@@ -1,4 +1,19 @@
 package org.generic;
 
 public class BoundedTypeParameter {
+
+    public static void main(String[] args) {
+        Integer[] intArr = {1, 5, 3, 2, 4, 10, 20, 14};
+        System.out.println(countGreaterThan(intArr, 5));
+    }
+
+    public static <T extends Comparable<T>> int countGreaterThan(T[] elementArray, T target) {
+        if (elementArray.length == 0) return 0;
+        int count = 0;
+        for (T e : elementArray) {
+            if (e.compareTo(target) > 0)
+                count ++;
+        }
+        return count;
+    }
 }
